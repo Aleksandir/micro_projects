a542c39ae54412a2b6751f83870055f2cf13dc7c
Aleks B <aleksandir@me.com>
2023-10-25 22:21:31 +1030

    Feat: Can roll a set number of dice

--- Die roll simulator/Main.py
+++ Die roll simulator/Main.py
@@ -1,1 +1,33 @@
+from curses.ascii import isdigit
+import sys
 from art import die_face
+from random import randint
+from os import system
+
+
+class Die:
+    def __init__(self):
+        faces = ["", 1, 2, 3, 4, 5, 6]
+
+    def roll(self, die_count):
+        for i in range(die_count):
+            print(die_face[randint(1, 6)], end="")
+            print("\n")
+
+
+die = Die()
+roll_again = "Y"
+while roll_again == "Y":
+    system("clear")
+    die_count = input("How many dice would you like to roll? (0 to quit) ")
+    while not die_count.isdigit():
+        die_count = input("Invalid input. Please enter a valid number: ")
+    die_count = int(die_count)
+    if die_count == 0:
+        print("Goodbye!")
+        break
+
+    die.roll(die_count)
+    roll_again = input("Would you like to roll again? (Y/N) ").upper()
+    while roll_again not in ["Y", "N"]:
+        roll_again = input("Invalid input. Please enter Y or N: ").upper()
