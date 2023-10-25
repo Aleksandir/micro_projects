26ce80135081e6d704ed4cc061af39a2c43bc20c
Aleks B <aleksandir@me.com>
2023-10-25 23:22:16 +1030

    Feat: print face of dice rolled on the same line

--- Die roll simulator/Main.py
+++ Die roll simulator/Main.py
@@ -1,17 +1,34 @@
-from art import die_face
+from art import die_face, DIE_HEIGHT, DIE_WIDTH, DIE_FACE_SEPARATOR
 from random import randint
 from os import system
 
 
 class Die:
+    def __init__(self):
+        self.die_face = die_face
+        self.die_height = DIE_HEIGHT
+        self.die_width = DIE_WIDTH
+        self.die_face_separator = DIE_FACE_SEPARATOR
+
     def roll(self, die_count):
         count = 0
+        dice_rolled = []
         for i in range(die_count):
-            face = randint(1, 6)
-            print(die_face[face], end="")
-            count += face
-        print()
+            roll = randint(1, 6)
+            dice_rolled.append(roll)
+
+            count += roll
+        self.print_die_face(dice_rolled)
         return count
+
+    def print_die_face(self, dice_rolled):
+        # loop through the height of the die
+        # print the top of each die face separated by the die face separator
+        # when at the end of the row, print a newline and move on to the next line
+        for i in range(self.die_height):
+            for die in dice_rolled:
+                print(self.die_face[die][i], end=self.die_face_separator)
+            print()
 
 
 def clear_screen():
