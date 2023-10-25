648f4c61baae90c347bf754c79485d16c7c7afa6
Aleks B <aleksandir@me.com>
2023-10-25 22:46:33 +1030

    Refactor: moved clear screen to function

--- Die roll simulator/Main.py
+++ Die roll simulator/Main.py
@@ -1,4 +1,3 @@
-from curses.ascii import isdigit
 from art import die_face
 from random import randint
 from os import system
@@ -6,22 +5,33 @@ from os import system
 
 class Die:
     def roll(self, die_count):
+        count = 0
         for i in range(die_count):
-            print(die_face[randint(1, 6)], end="")
+            face = randint(1, 6)
+            print(die_face[face], end="")
+            count += face
         print()
+        return count
+
+
+def clear_screen():
+    system("clear")
+
+    print("Welcome to the die roll simulator!\n")
 
 
 die = Die()
 roll_again = "Y"
 while roll_again == "Y":
-    system("clear")
+    clear_screen()
 
     die_count = input("How many dice would you like to roll?: ")
     while not die_count.isdigit():
+        clear_screen()
         die_count = input("Invalid input. Please enter a valid number: ")
     die_count = int(die_count)
 
-    die.roll(die_count)
+    print(f"You rolled {die.roll(die_count)}!\n")
 
     roll_again = input("Would you like to roll again? (Y/N) ").upper()
     while roll_again not in ["Y", "N"]:
