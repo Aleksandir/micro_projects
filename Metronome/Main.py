0b402ab65b123802e47eddb08a805e4042bc56c5
Aleks B <aleksandir@me.com>
2023-10-26 20:46:05 +1030

    Refactor: Moved to class method

--- Metronome/Main.py
+++ Metronome/Main.py
@@ -1,37 +1,49 @@
-from time import time, sleep
+from curses.ascii import isdigit
+from time import sleep
 from subprocess import Popen
 import keyboard
-
-sound = ["afplay", "Metronome/beep.mp3"]
-
-# Set the desired number of loops per second
-loops_per_second = 60
-
-# Calculate the time interval between each loop
-loop_interval = 60 / loops_per_second
-
-# Define the action variable
-action = ""
+from os import system
 
 
-# Define a callback function that is called when the space key is pressed
-def on_space_pressed(event):
-    global action
-    action = "stop"
+class Metronome:
+    def __init__(self, bpm):
+        self.bpm = bpm
+        self.loop_interval = 60 / bpm
+        self.action = ""
+        self.sound = ["afplay", "Metronome/beep.mp3"]
+
+        # Register the callback function for the space key
+        keyboard.on_press_key("space", self.on_space_pressed)
+
+    # Define a callback function that is called when the space key is pressed
+    def on_space_pressed(self, event):
+        self.action = "stop"
+
+    def play_metronome(self):
+        while True:
+            # Check if the space key has been pressed
+            if self.action == "stop":
+                break
+
+            # Play the sound
+            Popen(self.sound)
+
+            # Wait for the remaining time in the loop interval
+            sleep(self.loop_interval)
 
 
-# Register the callback function for the space key
-keyboard.on_press_key("space", on_space_pressed)
+quit = ""
+bpm = input("Enter the BPM: ")
+while quit != "q":
+    print("Press space to stop the metronome.")
 
-# Start the loop
-print("Press space to stop the metronome")
-while True:
-    # Check if the space key has been pressed
-    if action == "stop":
-        break
+    metronome = Metronome(int(bpm))
+    metronome.play_metronome()
 
-    # Play the sound
-    Popen(sound)
+    quit = input("\nPress q to quit or the BPM to continue: ")
+    if quit.isdigit():
+        bpm = quit
+    else:
+        quit = str(quit)
 
-    # Wait for the remaining time in the loop interval
-    sleep(loop_interval)
+    system("clear")
