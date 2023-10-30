ad249fee5df40b3df15379b62cc740f71dc4eb63
Aleks B <aleksandir@me.com>
2023-10-30 21:50:38 +1030

    Feat: Formates user input based on name

---
+++ CS50/Regular expressions/Format.py
@@ -0,0 +1,18 @@
+import re
+
+# Ask the user for their name
+name = input("Name: ").strip()
+
+# Search for a last name, comma, first name
+matches = re.search(r"^(.+), (.+)$", name)
+
+# If the name matches the pattern, extract the last name and first name
+if matches:
+    last = matches.group(1)
+    first = matches.group(2)
+    name = f"{first} {matches}"
+
+    # or you could do name = matches.group(2) + " " + matches.group(1)
+
+# Print the greeting
+print(f"Hello, {name}!")
