ab3a15aecd433beabbe9b5bcc010cca8eec89ab7
Aleks B <aleksandir@me.com>
2023-11-05 09:20:30 +1030

    Update rustc_info.json

---
+++ count_to1m/rust/target/release/CountTo1m.d
@@ -0,0 +1,1 @@
+/Users/aleks/Documents/GIthub\ Repositories/micro_projects/count_to1m/rust/target/release/CountTo1m: /Users/aleks/Documents/GIthub\ Repositories/micro_projects/count_to1m/rust/src/main.rs
---
+++ count_to1m/rust/target/release/deps/CountTo1m-bb0900b0e825e96f.d
@@ -0,0 +1,5 @@
+/Users/aleks/Documents/GIthub Repositories/micro_projects/count_to1m/rust/target/release/deps/CountTo1m-bb0900b0e825e96f: src/main.rs
+
+/Users/aleks/Documents/GIthub Repositories/micro_projects/count_to1m/rust/target/release/deps/CountTo1m-bb0900b0e825e96f.d: src/main.rs
+
+src/main.rs:
