1963fe6af5f8354b30a1ff13f4820735d39f2ea6
Aleks B <aleksandir@me.com>
2023-11-05 14:46:53 +1030

    Refactor GUI design and import statements in Main.py

--- Metronome/Main.py
+++ Metronome/Main.py
@@ -1,8 +1,6 @@
-from time import sleep
+import tkinter as tk
 from subprocess import Popen
-import keyboard
-import os
-import PySimpleGUI as sg
+from time import sleep
 
 
 class Metronome:
@@ -35,46 +33,45 @@ class Metronome:
         self.loop_interval = 60 / value
 
 
+window = tk.Tk()
+
 # GUI design
-sg.theme("DarkAmber")
-layout = [
-    [sg.Text("Enter the BPM: ")],
-    [sg.InputText(key="bpm")],
-    [sg.Button("Start"), sg.Button("Stop")],
-    [sg.Button("quit")],
-]
-window = sg.Window("Metronome", layout)
+bpm = tk.Entry(width=10)
+start = tk.Button(text="Start")
+stop = tk.Button(text="Stop")
 
+# Layout
+window.title("Metronome")
+window.resizable(width=False, height=False)
+bpm.grid(column=0, row=0, columnspan=2)
+start.grid(column=0, row=1)
+stop.grid(column=1, row=1)
 
-def get_bpm():
-    while True:
-        bpm_str = input("Enter the BPM (or q to quit): ")
-        # checking if in string due to extra space when first checked
-        if "q" in bpm_str.lower():
-            return None
-        try:
-            bpm = int(bpm_str)
-            return bpm
-        except ValueError:
-            print("Invalid input. Please enter a valid integer or q to quit.")
+# def get_bpm():
+#     while True:
+#         bpm_str = input("Enter the BPM (or q to quit): ")
+#         # checking if in string due to extra space when first checked
+#         if "q" in bpm_str.lower():
+#             return None
+#         try:
+#             bpm = int(bpm_str)
+#             return bpm
+#         except ValueError:
+#             print("Invalid input. Please enter a valid integer or q to quit.")
 
 
 #! currently wont quit or change bpm after being called
 def main():
-    metronome = Metronome(60)
+    metronome = Metronome(0)
+    window.mainloop()
     while True:
-        event, values = window.read()
-        bpm = int(values["bpm"])
-        if event == sg.WINDOW_CLOSED:
-            break
-        elif event == "Start":
+        bpm = bpm.get()
+        if start:
             metronome.bpm = bpm
             metronome.loop_interval = 60 / bpm
             metronome.play_metronome()
-        elif event == "Stop":
+        elif stop:
             metronome.action = "stop"
-        elif event == "quit":
-            break
 
 
-main()
+# main()
