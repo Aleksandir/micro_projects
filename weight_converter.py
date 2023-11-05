4f692dc1173b4aaab623c4757473dd0ba2692e56
Aleks B <aleksandir@me.com>
2023-11-05 11:20:29 +1030

    Refactor weight conversion UI

--- weight_converter.py
+++ weight_converter.py
@@ -6,29 +6,34 @@ choice = None
 
 
 def lb_to_kg():
-    weight = kg_value.get()
+    weight = value.get()
     converted_weight = float(weight) * CONVERT_TO_KG
-    kg_value.delete(0, tk.END)
-    kg_value.insert(0, f"{round(converted_weight, 1)} Lb")
+    value.delete(0, tk.END)
+    value.insert(0, f"{round(converted_weight, 1)} Lb")
 
 
 def kg_to_lb():
-    weight = lb_value.get()
+    weight = value.get()
     converted_weight = float(weight) * CONVERT_TO_LB
-    lb_value.delete(0, tk.END)
-    lb_value.insert(0, f"{round(converted_weight, 1)} Kg")
+    value.delete(0, tk.END)
+    value.insert(0, f"{round(converted_weight, 1)} Kg")
 
 
 window = tk.Tk()
 window.resizable(width=False, height=False)
 window.title("Weight Converter")
 
-kg_text = tk.Label(text="kilograms: ")
-kg_text.grid(column=0, row=1)
-kg_value = tk.Entry()
-kg_value.grid(column=1, row=1)
-kg_button = tk.Button(text="Convert to pounds", command=lb_to_kg)
-kg_button.grid(column=2, row=1)
+converter_text = tk.Label(text="Weight:")
+kg_button = tk.Button(text="Pounds", command=lb_to_kg)
+lb_button = tk.Button(text="Kilograms", command=kg_to_lb)
+
+value = tk.Entry()
+
+converter_text.grid(column=0, row=1)
+value.grid(column=1, row=1, pady=10, padx=10)
+
+kg_button.grid(column=0, row=2)
+lb_button.grid(column=1, row=2)
 
 
 # lb_text = tk.Label(text="Enter the weight in pounds: ")
