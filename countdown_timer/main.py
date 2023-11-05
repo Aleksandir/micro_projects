7b965e92e868f3fc772815621fe6ecd6365c1569
Aleks B <aleksandir@me.com>
2023-11-05 15:31:53 +1030

    Create layout for timer code

---
+++ countdown_timer/main.py
@@ -0,0 +1,23 @@
+# import tkinter
+
+# create timer class
+# timer class has a start and stop method
+# stop method runs in a separate thread and calls the stop method of the timer object
+# timer class has a property that returns the time remaining and
+
+# start/stop should be able to start the timer, and press again to pause
+# reset should reset the timer to the original time and stop the timer
+
+# create timer object
+
+# create a window
+# create a data entry panel
+# create a start/stop button
+# create a reset button
+# create a label to display the timer
+# create a layout
+
+# count down pane in the middle top
+# start button in the middle left and stop in the middle right
+
+# window.mainloop()
