25abf0085b6eb2dccf6ed480b4e06548ba9b4d49
Aleks B <aleksandir@me.com>
2023-11-05 18:39:46 +1030

    Add timer functionality to countdown timer GUI

--- countdown_timer/main.py
+++ countdown_timer/main.py
@@ -1,9 +1,10 @@
+import datetime
 import threading
 import time
 import tkinter as tk
 
 
-class timer:
+class Timer:
     def __init__(self, time):
         self.time = time
         self.running = False
@@ -41,19 +42,47 @@ window = tk.Tk()
 window.title("Countdown Timer")
 window.resizable(False, False)
 
+timer = Timer(10)
+
 # create a data entry panel
 time_entry = tk.Entry(window, width=15)
 time_entry.grid(row=0, column=0, columnspan=2, padx=10)
 
-# create a start/stop button
-start_stop_button = tk.Button(window, text="Start")
+
+# logic to start/stop the timer toggle and logic
+def start_stop():
+    if start_stop_button["text"] == "Start":
+        if timer.time == 0:
+            timer.time = int(time_entry.get())
+        start_stop_button["text"] = "Stop"
+        timer.start()
+    else:
+        start_stop_button["text"] = "Start"
+        timer.stop()
+
+
+start_stop_button = tk.Button(window, width=5, text="Start", command=start_stop)
 start_stop_button.grid(row=1, column=0, padx=10)
 
-# create a reset button
-reset_button = tk.Button(window, text="Reset")
+
+# create a reset button and logic
+def reset():
+    timer.reset()
+    display_label["text"] = "00:00:00"
+    start_stop_button["text"] = "Start"
+
+
+reset_button = tk.Button(window, width=5, text="Reset", command=reset)
 reset_button.grid(row=1, column=1, padx=10)
 
+
 # create a label to display the timer
+def update_label():
+    display_label["text"] = timer.time
+    if timer.running:
+        display_label.after(1, update_label)
+
+
 display_label = tk.Label(window, text="00:00:00")
 display_label.grid(row=2, column=0, columnspan=2, padx=10)
 
