955ee214c887f0725eedc084accf19e56a09e963
Aleks B <aleksandir@me.com>
2023-11-06 22:31:22 +1030

    Delete unnecessary files and code

--- helloworld_rust/target/debug/deps/helloworld_rust-816a003f9ef91dac.d
+++
@@ -1,5 +0,0 @@
-/Users/aleks/Documents/GIthub Repositories/micro_projects/helloworld_rust/target/debug/deps/helloworld_rust-816a003f9ef91dac: src/main.rs
-
-/Users/aleks/Documents/GIthub Repositories/micro_projects/helloworld_rust/target/debug/deps/helloworld_rust-816a003f9ef91dac.d: src/main.rs
-
-src/main.rs:
--- helloworld_rust/target/debug/helloworld_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/aleks/Documents/GIthub\ Repositories/micro_projects/helloworld_rust/target/debug/helloworld_rust: /Users/aleks/Documents/GIthub\ Repositories/micro_projects/helloworld_rust/src/main.rs
