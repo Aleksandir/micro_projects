650c86b315c1648628fbfef7411ba94322e98574
Aleks B <aleksandir@me.com>
2023-11-06 22:32:45 +1030

    Add alternative method to count to one million

--- count_to1m/python.py
+++ count_to1m/python.py
@@ -8,6 +8,11 @@ start_time = time.time()
 for number in one_million:
     print(number)
 
+# count = 0
+# while count < 1000000:
+#     print(count)
+#     count += 1
+
 end_time = time.time()
 
 execution_time = end_time - start_time
