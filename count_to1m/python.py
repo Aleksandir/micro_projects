993569eb53d8f6cd4ed773c96c7cf210ff32b72f
Aleks B <aleksandir@me.com>
2023-11-07 10:32:57 +1030

    Add for and while loops to count to 1 million

--- count_to1m/python.py
+++ count_to1m/python.py
@@ -5,16 +5,24 @@ one_million = list(range(1, 1000001))
 
 start_time = time.time()
 
+count = 0
 for number in one_million:
-    print(number)
-
-# count = 0
-# while count < 1000000:
-#     print(count)
-#     count += 1
+    count += 1
+    if count == 1000000:
+        print("For loop done!")
 
 end_time = time.time()
+execution_time_for = end_time - start_time
 
-execution_time = end_time - start_time
+start_time = time.time()
+count = 0
+while count < 1000000:
+    count += 1
+    if count == 1000000:
+        print("While loop done!")
 
-print(f"The loop took {execution_time} seconds to execute.")
+end_time = time.time()
+execution_time_while = end_time - start_time
+
+print(f"for loop completed in {execution_time_for} seconds.")
+print(f"while loop completed in {execution_time_while} seconds.")
