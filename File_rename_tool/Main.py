9f87c143583f36f6c571dbf157dbef6bbe418f0a
Aleks B <aleksandir@me.com>
2023-11-08 18:48:47 +1030

    Add file renaming functionality to File_rename_tool

--- File_rename_tool/Main.py
+++ File_rename_tool/Main.py
@@ -1,3 +1,18 @@
 # file rename tool
 # text files can be found at /Users/aleks/Documents/_GIthub_Repositories/micro_projects/File_rename_tool/test_files
 # or relative File_rename_tool/test_files
+import os
+
+test_path = "/Users/aleks/Documents/_GIthub_Repositories/micro_projects/File_rename_tool/test_files"
+
+
+def rename(path, new_name):
+    for file in os.listdir(path):
+        if file.endswith(".txt"):
+            os.rename(os.path.join(path, file), os.path.join(path, new_name + file))
+        else:
+            continue
+        # rest of your code
+
+
+rename(test_path, "t")
