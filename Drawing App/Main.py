5630fe70b783bb89f47aa8718821e7a5312a4550
Aleks B <aleksandir@me.com>
2023-11-08 18:33:32 +1030

    Refactor code for better readability and add button class

--- Drawing App/Main.py
+++ Drawing App/Main.py
@@ -1,12 +1,13 @@
-# https://thepythoncode.com/article/make-a-drawing-program-with-python
-
 import ctypes
 import sys
 
 import pygame
 
-# Increase Dots Per inch so that the program can look sharp
-ctypes.windll.user32.SetProcessDPIAware(True)
+# import system
+
+# # Increase Dots Per inch so it looks sharper
+# if system.platform == "win32":
+#     ctypes.windll.shcore.SetProcessDpiAwareness(True)
 
 # Pygame Configuration
 pygame.init()
@@ -14,26 +15,92 @@ fps = 300
 fpsClock = pygame.time.Clock()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+
 font = pygame.font.SysFont("Arial", 20)
 
 # Variables
+
 # Our Buttons will append themself to this list
 objects = []
+
 # Initial color
 drawColor = [0, 0, 0]
+
 # Initial brush size
 brushSize = 30
 brushSizeSteps = 3
+
 # Drawing Area Size
 canvasSize = [800, 800]
 
 
 # Button Class
 class Button:
-    ...
+    def __init__(
+        self,
+        x,
+        y,
+        width,
+        height,
+        buttonText="Button",
+        onclickFunction=None,
+        onePress=False,
+    ):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclickFunction = onclickFunction
+        self.onePress = onePress
+
+        self.fillColors = {
+            "normal": "#ffffff",
+            "hover": "#666666",
+            "pressed": "#333333",
+        }
+
+        self.buttonSurface = pygame.Surface((self.width, self.height))
+        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+
+        self.alreadyPressed = False
+
+        objects.append(self)
+
+    def process(self):
+        mousePos = pygame.mouse.get_pos()
+
+        self.buttonSurface.fill(self.fillColors["normal"])
+        if self.buttonRect.collidepoint(mousePos):
+            self.buttonSurface.fill(self.fillColors["hover"])
+
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.buttonSurface.fill(self.fillColors["pressed"])
+
+                if self.onePress:
+                    self.onclickFunction()
+
+                elif not self.alreadyPressed:
+                    self.onclickFunction()
+                    self.alreadyPressed = True
+
+            else:
+                self.alreadyPressed = False
+
+        self.buttonSurface.blit(
+            self.buttonSurf,
+            [
+                self.buttonRect.width / 2 - self.buttonSurf.get_rect().width / 2,
+                self.buttonRect.height / 2 - self.buttonSurf.get_rect().height / 2,
+            ],
+        )
+        screen.blit(self.buttonSurface, self.buttonRect)
 
 
 # Handler Functions
+
+
 # Changing the Color
 def changeColor(color):
     global drawColor
@@ -91,6 +158,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     # Drawing the Buttons
     for object in objects:
         object.process()
@@ -102,21 +170,25 @@ while True:
     # Drawing with the mouse
     if pygame.mouse.get_pressed()[0]:
         mx, my = pygame.mouse.get_pos()
+
         # Calculate Position on the Canvas
         dx = mx - x / 2 + canvasSize[0] / 2
         dy = my - y / 2 + canvasSize[1] / 2
+
         pygame.draw.circle(
             canvas,
             drawColor,
             [dx, dy],
             brushSize,
         )
-        # Reference Dot
+
+    # Reference Dot
     pygame.draw.circle(
         screen,
         drawColor,
         [100, 100],
         brushSize,
     )
+
     pygame.display.flip()
     fpsClock.tick(fps)
