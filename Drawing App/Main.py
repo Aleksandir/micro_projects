f28d05c5ff34ff0c5dc07234bad228c813e3fe98
Aleks B <aleksandir@me.com>
2023-11-08 12:09:13 +1030

    Add variables for drawing app

--- Drawing App/Main.py
+++ Drawing App/Main.py
@@ -15,3 +15,14 @@ fpsClock = pygame.time.Clock()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 font = pygame.font.SysFont("Arial", 20)
+
+# Variables
+# Our Buttons will append themself to this list
+objects = []
+# Initial color
+drawColor = [0, 0, 0]
+# Initial brush size
+brushSize = 30
+brushSizeSteps = 3
+# Drawing Area Size
+canvasSize = [800, 800]
