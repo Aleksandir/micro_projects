2d5f408ad414c9bad21462f5e7f6db1618ec5837
Aleks B <aleksandir@me.com>
2023-11-08 21:38:48 +1030

    Add file renaming functionality to File_rename_tool/Main.py

--- File_rename_tool/Main.py
+++ File_rename_tool/Main.py
@@ -7,12 +7,22 @@ test_path = "/Users/aleks/Documents/_GIthub_Repositories/micro_projects/File_ren
 
 
 def rename(path, new_name):
+    # Create a variable to track the number of files
+    count = 0
+    # Loop over each file in the path
     for file in os.listdir(path):
+        # Check if the file ends with ".txt"
         if file.endswith(".txt"):
-            os.rename(os.path.join(path, file), os.path.join(path, new_name + file))
-        else:
-            continue
-        # rest of your code
+            # Increment the file count by 1
+            count += 1
+            # Create a new file name using the count variable
+            new_file_name = f"{new_name}{count}.txt"
+            # Create the old file path using the os.path.join() method
+            old_file_path = os.path.join(path, file)
+            # Create the new file path using the os.path.join() method
+            new_file_path = os.path.join(path, new_file_name)
+            # Rename the file using the os.rename() method
+            os.rename(old_file_path, new_file_path)
 
 
-rename(test_path, "t")
+rename(test_path, "hello")
