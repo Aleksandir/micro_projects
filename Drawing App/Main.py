1e71af1516516fa19998fb204d00960453e5be92
Aleks B <aleksandir@me.com>
2023-11-08 12:08:44 +1030

    Add Pygame configuration to Main.py

--- Drawing App/Main.py
+++ Drawing App/Main.py
@@ -7,3 +7,11 @@ import pygame
 
 # Increase Dots Per inch so that the program can look sharp
 ctypes.windll.user32.SetProcessDPIAware(True)
+
+# Pygame Configuration
+pygame.init()
+fps = 300
+fpsClock = pygame.time.Clock()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+font = pygame.font.SysFont("Arial", 20)
