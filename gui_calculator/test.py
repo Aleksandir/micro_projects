a72c1a102625e35479a6090855e6dffc03154348
Aleks B <aleksandir@me.com>
2023-11-09 16:45:49 +1030

    Additions and changes to calculator test cases

--- gui_calculator/test.py
+++ gui_calculator/test.py
@@ -13,6 +13,20 @@ class TestCalculation(unittest.TestCase):
         self.assertEqual(
             evaluate_calculation("2*3+)5("), "Error"
         )  # Mismatched parentheses
+        self.assertEqual(evaluate_calculation("2**3"), "8")  # Exponentiation
+        self.assertEqual(evaluate_calculation("2*3**2"), "18")  # Order of operations
+        self.assertEqual(
+            evaluate_calculation("2*(3**2)"), "18"
+        )  # Order of operations with parentheses
+        self.assertEqual(
+            evaluate_calculation("2*3**2+5"), "23"
+        )  # Order of operations with addition
+        self.assertEqual(
+            evaluate_calculation("2*(3**2+5)"), "28"
+        )  # Order of operations with parentheses and addition
+        self.assertEqual(evaluate_calculation("2*"), "Error")  # Incomplete expression
+        self.assertEqual(evaluate_calculation(""), "Error")  # Empty expression
+        self.assertEqual(evaluate_calculation("2/0"), "Error")  # Division by zero
 
 
 if __name__ == "__main__":
