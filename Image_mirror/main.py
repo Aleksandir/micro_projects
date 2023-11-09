4759dfc87fbb32efe18d90b0aa862d6663027072
Aleks B <aleksandir@me.com>
2023-11-09 21:16:13 +1030

    Add image mirroring and deletion functionality

--- Image_mirror/main.py
+++ Image_mirror/main.py
@@ -5,24 +5,49 @@ import cv2
 IMAGE_EXTENSIONS = [".jpeg", ".png", ".jpg"]
 
 
-def mirror_image(filepath):
-    # Read the image
-    img = cv2.imread(filepath)
+def mirror_image(directory):
+    """
+    Reads an image from the specified file path, mirrors it horizontally, and saves the mirrored image to a new file with the
+    '_mirror' suffix appended to the original filename.
 
-    # Mirror the image
-    mirrored_img = cv2.flip(img, 1)
+    Args:
+        filepath (str): The path to the image file to be mirrored.
 
-    # Get the filename and extension of the original image
-    filename, ext = os.path.splitext(filepath)
+    Returns:
+        int: The number of images that were mirrored.
+    """
+    files = os.listdir(directory)
 
-    # Construct the new filename with the _mirror suffix
-    new_filename = f"{filename}_mirror{ext}"
+    # Loop through the files and mirror any that end with an image extension
+    count = 0
+    for file in files:
+        if file.endswith(tuple(IMAGE_EXTENSIONS)):
+            # Read the image
+            image = cv2.imread(os.path.join(directory, file))
 
-    # Save the mirrored image
-    cv2.imwrite(new_filename, mirrored_img)
+            # Mirror the image horizontally
+            mirrored_image = cv2.flip(image, 1)
+
+            # Save the mirrored image with the '_mirror' suffix appended to the original filename
+            mirrored_filename = (
+                os.path.splitext(file)[0] + "_mirror" + os.path.splitext(file)[1]
+            )
+            cv2.imwrite(os.path.join(directory, mirrored_filename), mirrored_image)
+
+            count += 1
+    return count
 
 
 def delete_rotated_images(directory):
+    """
+    Deletes all rotated images in the specified directory.
+
+    Args:
+        directory (str): The directory to search for rotated images.
+
+    Returns:
+        int: The number of rotated images that were deleted.
+    """
     # Get a list of all files in the directory
     files = os.listdir(directory)
 
@@ -37,18 +62,37 @@ def delete_rotated_images(directory):
     return count
 
 
+def get_input(prompt, valid_inputs):
+    response = input(prompt)
+    while response not in valid_inputs:
+        response = input(prompt)
+    return response
+
+
+def get_path(prompt):
+    path = input(prompt)
+    while not os.path.isdir(path):
+        path = input(prompt)
+    return path
+
+
+path = ""
 while True:
     print("1. Delete mirrored images")
     print("2. Mirror images in a directory")
     print("3. Exit")
 
-    option = input("Enter an option: ")
-    while option not in ["1", "2", "3"]:
-        option = input("Enter an option: ")
+    option = get_input("Enter an option: ", ["1", "2", "3"])
 
-    path = input("Enter a path: ")
-    while not os.path.isdir(path):
-        path = input("Enter a path: ")
+    if option == "3":
+        break
+
+    if path == "":
+        path = get_path("Enter a path: ")
+    else:
+        print(f"Current path: {path}")
+        if get_input("Change path? (y/n): ", ["y", "n"]) == "y":
+            path = get_path("Enter a path: ")
 
     match option:
         case "1":
@@ -56,12 +100,5 @@ while True:
             print(f"Deleted {count} images.")
 
         case "2":
-            count = 0
-            for image in os.listdir(path):
-                if image.endswith(tuple(IMAGE_EXTENSIONS)):
-                    mirror_image(os.path.join(path, image))
-                    count += 1
+            count = mirror_image(path)
             print(f"Mirrored {count} images.")
-
-        case "3":
-            break
