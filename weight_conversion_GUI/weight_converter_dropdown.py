2ad079b3101675c241b03af8abc90fbc744a4fed
Aleks B <aleksandir@me.com>
2023-11-14 17:37:08 +1030

    Refactor weight conversion function to accept conversion type and conversion table as arguments

--- weight_conversion_GUI/weight_converter_dropdown.py
+++ weight_conversion_GUI/weight_converter_dropdown.py
@@ -1,70 +1,104 @@
 import tkinter as tk
 
 
-def convert():
+def convert(conversion_type, weight, conversion_table: dict):
     """
     Converts the weight value entered by the user based on the selected conversion type.
-    The converted weight is displayed in the GUI.
+    The converted weight is returned.
     """
-    conversion_type = clicked.get()
-    weight = float(value.get())
-    conversion_value = conversions[conversion_type]
+    weight = float(weight)
+    conversion_value = conversion_table[conversion_type]
     converted_weight = weight * conversion_value
-    value.delete(0, tk.END)
 
     if converted_weight < 1:
-        value.insert(0, f"{round(converted_weight, 3)} {conversion_type[-2:]}")
+        return num_separator(f"{round(converted_weight, 3)} {conversion_type[-2:]}")
     else:
-        value.insert(0, f"{round(converted_weight, 1)} {conversion_type[-2:]}")
+        return num_separator(f"{round(converted_weight, 1)} {conversion_type[-2:]}")
 
 
-conversions = {
-    "g to Kg": 0.001,
-    "g to Lb": 0.00220462,
-    "g to Oz": 0.035274,
-    "Kg to G": 1000,
-    "Kg to Lb": 2.20462,
-    "Kg to Oz": 35.274,
-    "Oz to Lb": 0.0625,
-    "Oz to Kg": 0.0283495,
-    "Lb to Kg": 0.453592,
-    "Lb to Oz": 16,
-}
-
-options = list(conversions.keys())
+def num_separator(num):
+    """
+    Takes a number and returns a string with commas seperating the thousands.
+    """
+    num = str(num)
+    if len(num) > 3:
+        num = num[:-3] + "," + num[-3:]
+    return num
 
 
-window = tk.Tk()
-window.resizable(width=False, height=False)
-window.title("Weight Converter")
+# conversions = {
+#     "g to Kg": 0.001,
+#     "g to Lb": 0.00220462,
+#     "g to Oz": 0.035274,
+#     "Kg to G": 1000,
+#     "Kg to Lb": 2.20462,
+#     "Kg to Oz": 35.274,
+#     "Oz to Lb": 0.0625,
+#     "Oz to Kg": 0.0283495,
+#     "Lb to Kg": 0.453592,
+#     "Lb to Oz": 16,
+# }
 
-# label text
-converter_text = tk.Label(text="Weight:")
-
-# drop down menu
-clicked = tk.StringVar(window)
-clicked.set(options[0])
-options = tk.OptionMenu(window, clicked, *options)
-
-# Whenever the value of clicked changes, delete all the text from the value Entry widget
-clicked.trace("w", lambda *args: value.delete(0, tk.END))
-
-# Data entry panel
-value = tk.Entry(width=10)
-
-# convert button
-convert_button = tk.Button(text="Convert", command=convert)
-
-# Layout
-# colum o
-converter_text.grid(column=0, row=1)
-
-# colum 2
-value.grid(column=1, row=1, pady=10, padx=10)
-
-# colum 3
-options.grid(column=0, row=3, columnspan=2)
-convert_button.grid(column=0, row=4, columnspan=2)
+# options = list(conversions.keys())
 
 
-window.mainloop()
+def main():
+    conversions = {
+        "g to Kg": 0.001,
+        "g to Lb": 0.00220462,
+        "g to Oz": 0.035274,
+        "Kg to G": 1000,
+        "Kg to Lb": 2.20462,
+        "Kg to Oz": 35.274,
+        "Oz to Lb": 0.0625,
+        "Oz to Kg": 0.0283495,
+        "Lb to Kg": 0.453592,
+        "Lb to Oz": 16,
+    }
+
+    options = list(conversions.keys())
+
+    window = tk.Tk()
+    window.resizable(width=False, height=False)
+    window.title("Weight Converter")
+
+    # label text
+    converter_text = tk.Label(text="Weight:")
+
+    # drop down menu
+    option = tk.StringVar(window)
+    option.set(options[0])
+    options = tk.OptionMenu(window, option, *options)
+
+    # Whenever the value of clicked changes, delete all the text from the value Entry widget
+    option.trace("w", lambda *args: value.delete(0, tk.END))
+
+    # Data entry panel
+    value = tk.Entry(width=10)
+
+    # Convert button
+    def convert_and_insert():
+        print(option.get())
+        print(value.get())
+        result = convert(option.get(), value.get(), conversions)
+        value.delete(0, tk.END)
+        value.insert(0, result)
+
+    convert_button = tk.Button(text="Convert", command=convert_and_insert)
+
+    # Layout
+    # colum o
+    converter_text.grid(column=0, row=1)
+
+    # colum 2
+    value.grid(column=1, row=1, pady=10, padx=10)
+
+    # colum 3
+    options.grid(column=0, row=3, columnspan=2)
+    convert_button.grid(column=0, row=4, columnspan=2)
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
