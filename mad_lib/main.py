32891156724e680992fb0866d7aa146dd789e96b
Aleks B <aleksandir@me.com>
2023-11-16 12:16:04 +1030

    Add mad lib game code

---
+++ mad_lib/main.py
@@ -0,0 +1,11 @@
+exclamation = input("Enter an exclamation (e.g. Wow!): ")
+
+adverb = input("Enter an adverb (e.g. quickly): ")
+
+noun = input("Enter a noun (e.g. dog): ")
+
+adjective = input("Enter an adjective (e.g. beautiful): ")
+
+print(
+    f"{exclamation}! he said {adverb} as he jumped into his convertible {noun} and drove off with his {adjective} wife."
+)
