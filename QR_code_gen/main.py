482f13f8981daea0d448e850602960eb021175a3
Aleks B <aleksandir@me.com>
2023-11-17 12:34:21 +1030

    Update QR code generation function

--- QR_code_gen/main.py
+++ QR_code_gen/main.py
@@ -5,9 +5,9 @@ import qrcode
 save_path = "/Users/aleks/Documents/_GIthub_Repositories/micro_projects/QR_code_gen"
 
 
-def encode_text(text: str):
-    return qrcode.make(text)
+def encode_text(text: str, save_path: str = save_path):
+    img = qrcode.make(text)
+    img.save(os.path.join(save_path, "QR.png"))
 
 
-img = encode_text("I love you most! <3")
-img.save(os.path.join(save_path, "QR.png"))
+encode_text("")
