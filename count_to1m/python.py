92025077b1c3e1c165bb4821aa12009671d81bba
Aleks B <aleksandir@me.com>
2023-11-19 09:27:50 +1030

    Refactor loop code to remove unnecessary print statements

--- count_to1m/python.py
+++ count_to1m/python.py
@@ -7,9 +7,10 @@ start_time = time.time()
 
 count = 0
 for number in one_million:
+    print(count)
     count += 1
-    if count == 1000000:
-        print("For loop done!")
+    # if count == 1000000:
+    #     print("For loop done!")
 
 end_time = time.time()
 execution_time_for = end_time - start_time
@@ -17,9 +18,10 @@ execution_time_for = end_time - start_time
 start_time = time.time()
 count = 0
 while count < 1000000:
+    print(count)
     count += 1
-    if count == 1000000:
-        print("While loop done!")
+    # if count == 1000000:
+    #     print("While loop done!")
 
 end_time = time.time()
 execution_time_while = end_time - start_time
